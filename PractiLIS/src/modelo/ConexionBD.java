216df333a4259a0d7934b5ad738355f04dbdec55
LuisGerardoRendon <50636600+LuisGerardoRendon@users.noreply.github.com>
2020-07-09 18:43:05 -0500

    Clase conexion a la base de datos

---
+++ PractiLIS/src/modelo/ConexionBD.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+
+    // Librería de MySQL
+    public String driver = "com.mysql.jdbc.Driver";
+
+    // Nombre de la base de datos
+    public String database = "Ejemplo_crud";
+    
+    // Host
+    public String hostname = "localhost";
+
+    // Puerto
+    public String port = "3306";
+
+    // Ruta de nuestra base de datos (desactivamos el uso de SSL con "?useSSL=false")
+    public String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false";
+
+    // Nombre de usuario
+    public String username = "root";
+
+    // Clave de usuario
+    public String password = "123456";
+
+    public Connection conectarMySQL() throws Exception{
+        Connection conn = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new Exception("Error en ConexionBD la causa es: " +  e.getCause().toString());
+        }
+
+        return conn;
+    }
+
+}
