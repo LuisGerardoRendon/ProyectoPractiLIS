f58912bf36aef25d428766c537ab0beb557bbc01
AldoColorado <aldocoloradocd@gmail.com>
2020-07-11 19:57:59 -0500

    ReporteDAO

--- PractiLIS/src/modelo/ReporteDAOImplements.java
+++ PractiLIS/src/modelo/ReporteDAOImplements.java
@@ -19,46 +19,46 @@ import javafx.collections.ObservableList;
  *
  * @author ALDO
  */
-public class ReporteDAOImplements implements ReporteDAO{
-   
+public class ReporteDAOImplements implements ReporteDAO {
+
    @Override
-   public boolean create(ReporteVO reporte) throws Exception{
-        boolean created =false;
-        PreparedStatement ps = null;
-        Connection con=null;
-        
-        try{
-            con=null;
-            ConexionBD cc=new ConexionBD();
-            con=cc.conectarMySQL();
-            
-            String sql = "INSERT INTO reporte (numero,horasReportadas, fechaCarga, estado, reporte, "
-                        + "fechaInicio, fechaFin, idExpediente) VALUES (?, ?, ?, ?, ?, ?,?,?)";
-            ps = con.prepareStatement(sql);
-            
-            ps.setInt(1, 0);
-            ps.setInt(2, reporte.getHorasReportadas());
-            ps.setString(3, reporte.getFechaCarga());
-            ps.setString(4, reporte.getEstado());
-            FileInputStream archivo = new FileInputStream(reporte.getReporte());
-            ps.setBlob(5, archivo);
-            ps.setString(6, reporte.getFechaInicio());
-            ps.setString(7, reporte.getFechaFin());
-            ps.setInt(8,1);
-            
-            ps.executeUpdate();
-            created=true;
-            con.close();
-            ps.close();
-            
-        }catch(SQLException e){
-            throw new Exception("Error en create SQLException " + e.getMessage());
-        }catch(NullPointerException e){
-            throw new Exception("Error en create NullPointerException " + e.getMessage());
-        }catch (Exception e) {
+   public boolean create(ReporteVO reporte, int idExpediente) throws Exception {
+      boolean created = false;
+      PreparedStatement ps = null;
+      Connection con = null;
+
+      try {
+         con = null;
+         ConexionBD cc = new ConexionBD();
+         con = cc.conectarMySQL();
+
+         String sql = "INSERT INTO reporte (numero,horasReportadas, fechaCarga, estado, reporte,"
+                 + "fechaInicio, fechaFin"+",'"+ idExpediente+"'"+") VALUES (?, ?, ?, ?, ?, ?,?,?)";
+         ps = con.prepareStatement(sql);
+
+         ps.setInt(1, 0);
+         ps.setInt(2, reporte.getHorasReportadas());
+         ps.setString(3, reporte.getFechaCarga());
+         ps.setString(4, reporte.getEstado());
+         FileInputStream archivo = new FileInputStream(reporte.getReporte());
+         ps.setBlob(5, archivo);
+         ps.setString(6, reporte.getFechaInicio());
+         ps.setString(7, reporte.getFechaFin());
+         ps.setInt(8, 1);
+
+         ps.executeUpdate();
+         created = true;
+         con.close();
+         ps.close();
+
+      } catch (SQLException e) {
+         throw new Exception("Error en create SQLException " + e.getMessage());
+      } catch (NullPointerException e) {
+         throw new Exception("Error en create NullPointerException " + e.getMessage());
+      } catch (Exception e) {
          throw new Exception("Error en create Exception " + e.getMessage());
-        }finally{
-           try {
+      } finally {
+         try {
             if (ps != null) {
                ps.close();
             }
@@ -70,34 +70,35 @@ public class ReporteDAOImplements implements ReporteDAO{
             }
          } catch (Exception e) {
          };
-        }
-        return created;
-    }
-   @Override
-    public ObservableList<ReporteVO> recuperarReportes(String periodo, String matricula)
-            throws Exception {
-        String sql = crearSQLRecuperarReportes(periodo, matricula);
-        ObservableList<ReporteVO> reportesRecuperados = FXCollections.observableArrayList();
-        Connection con = null;
-        Statement stm = null;
-        ConexionBD cc = new ConexionBD();
-        ResultSet rs = null;
-        try {
-            con = cc.conectarMySQL();
-            stm = con.createStatement();
-            rs = stm.executeQuery(sql);
-            while (rs.next()) {
-                int numero = rs.getInt("numero");
-                int horasReportadas = rs.getInt("horasReportadas");
-                String fechaCarga = rs.getString("fechaCarga");
-                ReporteVO reporteRecuperado = new ReporteVO(numero, horasReportadas, fechaCarga);
-                reportesRecuperados.add(reporteRecuperado);
-            }
+      }
+      return created;
+   }
 
-            con.close();
-            stm.close();
-            rs.close();
-        } catch (SQLException e) {
+   @Override
+   public ObservableList<ReporteVO> recuperarReportes(String periodo, String matricula)
+           throws Exception {
+      String sql = crearSQLRecuperarReportes(periodo, matricula);
+      ObservableList<ReporteVO> reportesRecuperados = FXCollections.observableArrayList();
+      Connection con = null;
+      Statement stm = null;
+      ConexionBD cc = new ConexionBD();
+      ResultSet rs = null;
+      try {
+         con = cc.conectarMySQL();
+         stm = con.createStatement();
+         rs = stm.executeQuery(sql);
+         while (rs.next()) {
+            int numero = rs.getInt("numero");
+            int horasReportadas = rs.getInt("horasReportadas");
+            String fechaCarga = rs.getString("fechaCarga");
+            ReporteVO reporteRecuperado = new ReporteVO(numero, horasReportadas, fechaCarga);
+            reportesRecuperados.add(reporteRecuperado);
+         }
+
+         con.close();
+         stm.close();
+         rs.close();
+      } catch (SQLException e) {
          throw new Exception("Error en create SQLException " + e.getMessage());
       } catch (NullPointerException e) {
          throw new Exception("Error en create NullPointerException " + e.getMessage());
@@ -117,23 +118,24 @@ public class ReporteDAOImplements implements ReporteDAO{
          } catch (Exception e) {
          };
       }
-        return reportesRecuperados;
-    }
-    @Override
-    public String crearSQLRecuperarReportes(String periodo, String matricula)throws Exception {
-        String sql = "SELECT Reporte.numero,Reporte.horasReportadas,"
-                + "Reporte.fechaCarga, Reporte.Estado,Reporte.reporte,"
-                + " Reporte.fechaInicio, Reporte.fechaFin";
-        sql += " FROM Estudiante INNER JOIN Asignacion on Estudiante.matricula "
-                + "= Asignacion.matriculaEstudiante";
-        sql += " INNER JOIN Expediente on Asignacion.idAsignacion = "
-                + "Expediente.idAsignacion ";
-        sql += " INNER JOIN Reporte on Expediente.idExpediente = "
-                + "Reporte.idExpediente ";
-        sql += " WHERE Asignacion.preriodo = " + "'" + periodo + "'";
-        sql += " AND Asignacion.matriculaEstudiante =" + "'" + matricula + "';";
-        System.out.println(sql);
-        return sql;
-    }
-    
+      return reportesRecuperados;
+   }
+
+   @Override
+   public String crearSQLRecuperarReportes(String periodo, String matricula) throws Exception {
+      String sql = "SELECT Reporte.numero,Reporte.horasReportadas,"
+              + "Reporte.fechaCarga, Reporte.Estado,Reporte.reporte,"
+              + " Reporte.fechaInicio, Reporte.fechaFin";
+      sql += " FROM Estudiante INNER JOIN Asignacion on Estudiante.matricula "
+              + "= Asignacion.matriculaEstudiante";
+      sql += " INNER JOIN Expediente on Asignacion.idAsignacion = "
+              + "Expediente.idAsignacion ";
+      sql += " INNER JOIN Reporte on Expediente.idExpediente = "
+              + "Reporte.idExpediente ";
+      sql += " WHERE Asignacion.preriodo = " + "'" + periodo + "'";
+      sql += " AND Asignacion.matriculaEstudiante =" + "'" + matricula + "';";
+      System.out.println(sql);
+      return sql;
+   }
+
 }
